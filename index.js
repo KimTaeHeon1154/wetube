bbb7fe59abb221f1b7e915b63e8eb90efb41016d
Tae Heon Kim <taeheonkim@Taeui-MacBookPro.local>
2020-01-14 13:58:48 +0900

    1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("HI")
+import express from "express";
+const app = express();
+
+const handleListening = () => console.log("Listening on: http://localhost:4000 ");
+
+const handleHome = (req, res) => res.send('Hello from Homie');
+
+
+const handleProfile = (req, res) => res.send("You are on my profile");
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(4000, handleListening);
