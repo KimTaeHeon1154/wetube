4226516a5dec4b51a4156143dcba9eb9bf141109
Tae Heon Kim <taeheonkim@Taeui-MacBookPro.local>
2020-01-15 17:56:30 +0900

    2

--- app.js
+++ app.js
@@ -7,18 +7,22 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddleware } from "./middlewares";
 //자식 디렉토리로 갈때는 ./를 쓴다.//
 
 //express import한 걸 상수로 변환
 const app = express();
 
-//각종 middleware들
+//각종 middleware들 (순서 중요함)
+app.use(helmet()); //보안 관련
 app.set("view engine", "pug"); //원래는 view engine 따로 없는데, pug사용하기 위해 등록!
 app.use(cookieParser()); //쿠키 전달 관련
 app.use(bodyParser.json()); //사용자 정보 전달한 거 검사
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); //보안 관련
 app.use(morgan("dev")); //application에서 일어나는 일 모두 logging
+
+//local 변수들을 global하게 쓰기 위한 middleware
+app.use(localsMiddleware); //middlewares.js에 있는 변수들을 사용하겠다
 
 //Routers
 app.use(routes.home, globalRouter);
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,11 +1,11 @@
 //video 관련 모델에 쓰이는 함수(로직) 모음 파일
 //globalRouter.js, videoRouter.js에 있는 함수들에 들어간다.
 
-export const home = (req, res) => res.render("home"); //render함수를 쓰면, views 폴더의 home.pug파일을 찾아서 해당 html을 보여준다!!!
-export const search = (req, res) => res.render("search");
-
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: "Home" }); //render함수를 쓰면, views 폴더의 home.pug파일을 찾아서 해당 html을 보여준다!!!
+// 두번째 객체인 {}안의 내용 보면, pageTitle 변수에 저장할 수 있다. 이 변수는 home.pug에서만 global하게 쓰이는 변수이다. (각 템플릿마다 이렇게 전역적 변수를 설정해줄 수 있다.)
+export const search = (req, res) => res.render("search", { pageTitle: "Search" });
+export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
+export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const videoDetail = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideo = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideo = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
