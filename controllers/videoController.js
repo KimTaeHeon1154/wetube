194fc46e8243c648c19ad3cbcdcfc5358447e6f2
Tae Heon Kim <taeheonkim@192.168.nate.com>
2020-01-18 15:30:13 +0900

    4

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,13 @@
 //video 관련 모델에 쓰이는 함수(로직) 모음 파일
 //globalRouter.js, videoRouter.js에 있는 함수들에 들어간다.
 
-export const home = (req, res) => res.render("home", { pageTitle: "Home" }); //render함수를 쓰면, views 폴더의 home.pug파일을 찾아서 해당 html을 보여준다!!!
+import { videos } from "../db"
+
+//render함수를 쓰면, views 폴더의 home.pug파일(해당하는)을 찾아서 해당 html을 보여주고, export로 인해 해당 pug파일에 변수들을 전달해준다.!!!
+export const home = (req, res) => {
+    res.render("home", { pageTitle: "Home", videos });
+    //home.pug 실행할 때, db.js에서 import한 videos array도 전달한다.
+};
 // 두번째 객체인 {}안의 내용 보면, pageTitle 변수에 저장할 수 있다. 이 변수는 home.pug에서만 global하게 쓰이는 변수이다. (각 템플릿마다 이렇게 전역적 변수를 설정해줄 수 있다.)
 
 export const search = (req, res) => {
@@ -10,8 +16,6 @@ export const search = (req, res) => {
     res.render("search", { pageTitle: "Search", searchingBy });
     // searchingBy 변수도 전달함
 };
-
-export const videos = (req, res) => res.render("videos", { pageTitle: "Videos" });
 
 export const upload = (req, res) => res.render("upload", { pageTitle: "Upload" });
 
